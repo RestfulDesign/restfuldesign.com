4efa78293c6f57721d07430a58612623f192a7e9
KOE <oskar@restfuldesign.com>
2015-03-26 16:45:55 +0100

    Added new header background image and deleted old images

--- gulpfile.js
+++ gulpfile.js
@@ -85,7 +85,7 @@ gulp.task('sass', function () {
 gulp.task('watch', function () {
   gulp.watch('_scss/*.scss', ['sass']);
   gulp.watch(['src/*.js', 'src/**/*.js'], ['scripts']);
-  gulp.watch(['index.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+  gulp.watch(['index.html', 'design-guide/*.html', 'design/*.html', 'ecommerce/*.html', 'development/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
 /**
