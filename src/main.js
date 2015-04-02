d9d33ea850a4b168e250627f339ade19e63ae9f0
KOE <oskar@restfuldesign.com>
2015-04-02 18:06:45 +0200

    Added new header background image. Bug fixes to quote rotator

--- src/main.js
+++ src/main.js
@@ -21,31 +21,25 @@ window.addEventListener('load', function() {
 function CarouselInViewport() {
   
   var qoutes = getChildrenOf('quotes'),
-      quote = document.querySelector(".quote-progress"),
-      quoteInterval;
+      quote = document.querySelector(".quote-progress");
       
   if (verge.inViewport(quote)) {
     Carousel(qoutes, 15000, 0);
     console.log(verge.inViewport(quote))
-  } else {
-    console.log(verge.inViewport(quote))
-
-    clearInterval(quoteInterval);
-  }
-  
-  function Carousel(e,s,c,m,i) {
-    var m = e.length-1,
-        c = c||0;
-        
-    quoteInterval = setInterval(f,s);
-    function f() {
-      for(i=m;~i;i--)e[i].className=i==c ? "t":"";c=++c%(m+1);
-    } 
-    f();   
-  }
+  } 
 }
 
-
+function Carousel(e,s,c,m,i) {
+  var m = e.length-1,
+      c = c||0;
+      
+  
+  function f() {
+    for(i=m;~i;i--)e[i].className=i==c ? "t":"";c=++c%(m+1);
+  } 
+  f();
+  setInterval(f,s);
+}
 
 function changeBackgroundColor(id) {
   var y = window.pageYOffset || document.documentElement.scrollTop;
