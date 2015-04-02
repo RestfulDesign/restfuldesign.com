ea79780afaf938f50460923646e7fd5b635693cc
KOE <oskar@restfuldesign.com>
2015-04-02 11:37:23 +0200

    Changes to quote carousel code

--- src/main.js
+++ src/main.js
@@ -3,34 +3,61 @@
  */
 new WOW().init();
 
-var Carousel = function(e,s,c,m,i) {
-    m=e.length-1,c=c||0;
-    function f() {
-      for(i=m;~i;i--)e[i].className=i==c ? "t":"";c=++c%(m+1);
-    } 
-    f();
-    setInterval(f,s);
-}
 window.addEventListener('load', function() {
-  var qoutes = getChildrenOf('quotes'),
-      topNav = document.getElementById("nav");
+  var topNav = document.getElementById("nav");
   
-  Carousel(qoutes, 15000, 0);
+
+  changeBackgroundColor(topNav);
+  
+  CarouselInViewport()
   
   // On scroll actions
   window.onscroll = function() {
-    var y = window.pageYOffset || document.documentElement.scrollTop;
-    if (y > 20) {
-      topNav.className = "fixed"; 
-    } else {      
-      topNav.className = "";      
-    }
-  } 
-
+    changeBackgroundColor(topNav);
+    CarouselInViewport();
+  }
 }, false)
 
+function CarouselInViewport() {
+  
+  var qoutes = getChildrenOf('quotes'),
+      quote = document.querySelector(".quote-progress"),
+      quoteInterval;
+      
+  if (verge.inViewport(quote)) {
+    Carousel(qoutes, 15000, 0);
+    console.log(verge.inViewport(quote))
+  } else {
+    console.log(verge.inViewport(quote))
+
+    clearInterval(quoteInterval);
+  }
+  
+  function Carousel(e,s,c,m,i) {
+    var m = e.length-1,
+        c = c||0;
+        
+    quoteInterval = setInterval(f,s);
+    function f() {
+      for(i=m;~i;i--)e[i].className=i==c ? "t":"";c=++c%(m+1);
+    } 
+    f();   
+  }
+}
+
+
+
+function changeBackgroundColor(id) {
+  var y = window.pageYOffset || document.documentElement.scrollTop;
+  if (y > 20) {
+    id.className = "fixed"; 
+  } else {      
+    id.className = "";      
+  }
+}
+
 function getChildrenOf(id) { 
-    return [].filter.call(document.getElementById(id).childNodes, function(y) {
-        return (y.nodeType === 1)
-    }); 
+  return [].filter.call(document.getElementById(id).childNodes, function(y) {
+    return (y.nodeType === 1)
+  }); 
 };
